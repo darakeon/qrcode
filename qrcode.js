bf6f0b0538741c5d43a78a329acbb4210fd80b61
Dara Keon <lc.sales.duarte@gmail.com>
2025-04-20 01:39:17 +0100

    add default qrcode as my resume

--- qrcode.js
+++ qrcode.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-	const text = window.location.search.substring(1)
+	const text = window.location.search.substring(1) || 'https://darakeon.com'
 	const correctLevel = 0
 	$('#qrcode').qrcode({ text, correctLevel })
 })
